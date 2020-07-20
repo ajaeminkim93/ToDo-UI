3bd48a3cff978bf783620ea90ab85fef413918ce
Jae(Andrew) Kim <jae.m.kim@wsu.edu>
2020-07-20 16:03:37 -0700

    Created a new route guard service, to authenticate to route to certain urls. Reroute to login if you route somewhere else when not logged in.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,15 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { ErrorComponent } from './error/error.component';
 import { ListToDosComponent } from './list-to-dos/list-to-dos.component';
 import { LogoutComponent } from './logout/logout.component';
+import { RouteGuardService } from './service/route-guard.service';
 
 // defining the literal routes
 const routes: Routes = [ 
-  { path: '', component: LoginComponent },
+  { path: '', component: LoginComponent }, // activate routeguard
   { path: 'login', component: LoginComponent },
-  { path: 'welcome/:name', component: WelcomeComponent },
-  { path: 'todos', component: ListToDosComponent },
-  { path: 'logout', component: LogoutComponent },
+  { path: 'welcome/:name', component: WelcomeComponent, canActivate: [RouteGuardService] },
+  { path: 'todos', component: ListToDosComponent, canActivate: [RouteGuardService] },
+  { path: 'logout', component: LogoutComponent, canActivate: [RouteGuardService] },
   { path: '**', component: ErrorComponent }
   
 ];
